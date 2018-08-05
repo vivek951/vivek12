81235d7a7ffb02e431ba623b88a7720baee489ef
root <root@ip-172-31-90-173.ec2.internal>
2018-08-05 09:58:02 +0000

    fe

---
+++ src/main/java/cjp/in/com/App.java
@@ -0,0 +1,13 @@
+package cjp.in.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
